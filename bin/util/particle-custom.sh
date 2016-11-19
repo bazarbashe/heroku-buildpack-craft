f16e38f32e4e41b94fe6f35f4dfe9af25b2eaff4
Zach Supalla <zsupalla@gmail.com>
2016-11-18 18:30:30 -0800

    Remove npm stuff because we don't have access to npm

--- bin/util/particle-custom.sh
+++ bin/util/particle-custom.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 particle_custom_stuff() {
-  status "Processing gulp files..."
-  npm install
-  gulp build --env production
-
   status "Loading in the database..."
   bin/load-db-into-jawsdb
 }
